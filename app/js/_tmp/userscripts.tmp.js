c0b20208fd898e32bdeafbb963f307d141cb83f6
Alexander Pletnev <pletnev.a.a@yandex.ru>
2021-07-07 22:54:25 +0300

    Добавил ресурсы. Сделал адаптив шапки и о компании

--- app/js/_tmp/userscripts.tmp.js
+++ app/js/_tmp/userscripts.tmp.js
@@ -1,4 +1,54 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', function () {// Custom JS
+document.addEventListener('DOMContentLoaded', function () {
+  /* Hamburher menu */
+  var menu = document.querySelector('.mobile-menu');
+  /* Aside menu */
+
+  var burger = document.querySelector('.burger');
+  /* desktop btn aside menu  */
+
+  var menuClose = document.querySelector('.mobile-menu__close');
+  /* close btn for aside menu */
+
+  var overlay = document.querySelector('.overlay');
+  /* overlay for aside menu */
+
+  /* lock screen scroll */
+
+  var lockScroll = function lockScroll() {
+    document.body.classList.add('lock');
+  };
+  /* unlock screen scroll */
+
+
+  var unlockScroll = function unlockScroll() {
+    document.body.classList.remove('lock');
+  };
+  /* Open menu */
+
+
+  burger.addEventListener('click', function () {
+    burger.classList.add('hide');
+    menu.classList.add('open');
+    overlay.classList.add('open');
+    lockScroll();
+  });
+  /* Close aside menu */
+
+  menuClose.addEventListener('click', function () {
+    burger.classList.remove('hide');
+    menu.classList.remove('open');
+    overlay.classList.remove('open');
+    unlockScroll();
+  });
+  /* Remove overlay and unlock screen scroll */
+
+  overlay.addEventListener('click', function () {
+    burger.classList.remove('hide');
+    menu.classList.remove('open');
+    overlay.classList.remove('open');
+    unlockScroll();
+  });
+  /* End of Hamburher menu */
 });
--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-"use strict";
-
-document.addEventListener('DOMContentLoaded', function () {// Custom JS
-});
+	/* Hamburher menu */
+	const menu = document.querySelector('.mobile-menu'); /* Aside menu */
+	const burger = document.querySelector('.burger'); /* desktop btn aside menu  */
+	const menuClose = document.querySelector('.mobile-menu__close'); /* close btn for aside menu */
+	const overlay = document.querySelector('.overlay'); /* overlay for aside menu */
+  
+	/* lock screen scroll */
+	const lockScroll = () => {
+	  document.body.classList.add('lock');
+	}
+	/* unlock screen scroll */
+	const unlockScroll = () => {
+	  document.body.classList.remove('lock');
+	}
+  
+	/* Open menu */
+	burger.addEventListener('click', () => {
+	  burger.classList.add('hide');
+	  menu.classList.add('open');
+	  overlay.classList.add('open');
+	  lockScroll();
+	});
+	/* Close aside menu */
+	menuClose.addEventListener('click', () => {
+	  burger.classList.remove('hide');
+	  menu.classList.remove('open');
+	  overlay.classList.remove('open');
+	  unlockScroll();
+	});
+	/* Remove overlay and unlock screen scroll */
+	overlay.addEventListener('click', () => {
+	  burger.classList.remove('hide');
+	  menu.classList.remove('open');
+	  overlay.classList.remove('open');
+	  unlockScroll();
+	});
+	/* End of Hamburher menu */
+})
